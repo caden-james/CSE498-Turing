ee4bf9c9f239afb4823457a932abd67c94900d2b
han <dalehan1@msu.edu>
2025-04-21 15:18:51 -0400

    Convert DynamicString to header‑only; remove .cpp

--- Dev/Classes/Backend Support/DynamicString/DynamicString.hpp
+++ Dev/Classes/Backend Support/DynamicString/DynamicString.hpp
@@ -1,6 +1,3 @@
-#ifndef DYNAMICSTRING_H
-#define DYNAMICSTRING_H
-
 /**
  * @author Hannah Dale
  * @file DynamicString.h
@@ -19,6 +16,7 @@
  *        a full string on demand by concatenating all segments.
  */
 namespace cse {
+
 class DynamicString {
 public:
     /// Alias to represent a dynamic text segment
@@ -44,6 +42,61 @@ private:
     /// The container that holds both string and function segments
     std::vector<std::variant<std::string, str_fun_t>> mSegments;
 };
+
+
+/************************************
+ * Inline Method Implementations
+ *  
+ * We mark these methods inline so that, when this non‑template
+ * class is defined in a header and included by multiple TUs,
+ * the linker will merge duplicate definit
+ ************************************/
+ inline DynamicString::DynamicString()
+ : mSegments() { }
+
+inline DynamicString::DynamicString(const std::string &s) {
+ mSegments.emplace_back(s);
 }
 
-#endif
+inline DynamicString::DynamicString(str_fun_t f) {
+ mSegments.emplace_back(f);
+}
+
+inline std::string DynamicString::ToString() const {
+ std::string result;
+ for (auto &segment : mSegments) {
+     std::visit([&result](auto &segValue) {
+         using segType = std::decay_t<decltype(segValue)>;
+         if constexpr (std::is_same_v<segType, std::string>) {
+             result += segValue;
+         } else {
+             result += segValue();
+         }
+     }, segment);
+ }
+ return result;
+}
+
+inline DynamicString &DynamicString::Append(const std::string &staticStr) {
+ mSegments.emplace_back(staticStr);
+ return *this;
+}
+
+inline DynamicString &DynamicString::Append(str_fun_t func) {
+ mSegments.emplace_back(func);
+ return *this;
+}
+
+inline DynamicString operator+(const DynamicString &lhs, const std::string &rhs) {
+ DynamicString result = lhs;
+ result.Append(rhs);
+ return result;
+}
+
+inline DynamicString operator+(const DynamicString &lhs, DynamicString::str_fun_t rhs) {
+ DynamicString result = lhs;
+ result.Append(rhs);
+ return result;
+}
+
+} // namespace cse
